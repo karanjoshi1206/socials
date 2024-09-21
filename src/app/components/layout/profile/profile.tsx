adb53de10903fd78d5f18cb327c6032cf12899fe
karanjoshi1206 <joshikaran392@gmail.com>
2024-09-22 04:45:14 +0530

    added PWA support

--- src/app/components/layout/profile/profile.tsx
+++ src/app/components/layout/profile/profile.tsx
@@ -24,50 +24,50 @@ const Profile = () => {
     fetchUserData();
   }, [session]);
   if (status === "loading") return <div>Loading...</div>;
-  if (session?.user) {
-    return (
-      <>
-        {!isEditing && (
-          <div className="min-h-screen flex items-center justify-center flex-col bg-gray-50 dark:bg-gray-900">
-            <div className="bg-white dark:bg-gray-800 shadow-md rounded-lg p-8 max-w-md w-full">
-              {/* Profile Information */}
-              <div className="text-center mb-6">
-                <h1 className="text-3xl font-bold text-gray-800 dark:text-white">{userData?.name}</h1>
-                <p className="text-sm text-gray-500 dark:text-gray-400">@{userData?.userName}</p>
+  // if (session?.user) {
+  return (
+    <>
+      {!isEditing && (
+        <div className="min-h-screen flex items-center justify-center flex-col bg-gray-50 dark:bg-gray-900">
+          <div className="bg-white dark:bg-gray-800 shadow-md rounded-lg p-8 max-w-md w-full">
+            {/* Profile Information */}
+            <div className="text-center mb-6">
+              <h1 className="text-3xl font-bold text-gray-800 dark:text-white">{userData?.name}</h1>
+              <p className="text-sm text-gray-500 dark:text-gray-400">@{userData?.userName}</p>
+            </div>
+
+            {/* Details */}
+            <div className="space-y-4">
+              {/* Email */}
+              <div className="flex items-center justify-between bg-gray-100 dark:bg-gray-700 p-4 rounded-lg">
+                <span className="text-gray-600 dark:text-gray-300">Email</span>
+                <span className="text-gray-800 dark:text-white">{userData?.email}</span>
               </div>
 
-              {/* Details */}
-              <div className="space-y-4">
-                {/* Email */}
-                <div className="flex items-center justify-between bg-gray-100 dark:bg-gray-700 p-4 rounded-lg">
-                  <span className="text-gray-600 dark:text-gray-300">Email</span>
-                  <span className="text-gray-800 dark:text-white">{userData?.email}</span>
-                </div>
+              {/* Name */}
+              <div className="flex items-center justify-between bg-gray-100 dark:bg-gray-700 p-4 rounded-lg">
+                <span className="text-gray-600 dark:text-gray-300">Name</span>
+                <span className="text-gray-800 dark:text-white">{userData?.name}</span>
+              </div>
 
-                {/* Name */}
-                <div className="flex items-center justify-between bg-gray-100 dark:bg-gray-700 p-4 rounded-lg">
-                  <span className="text-gray-600 dark:text-gray-300">Name</span>
-                  <span className="text-gray-800 dark:text-white">{userData?.name}</span>
-                </div>
-
-                {/* Username */}
-                <div className="flex items-center justify-between bg-gray-100 dark:bg-gray-700 p-4 rounded-lg">
-                  <span className="text-gray-600 dark:text-gray-300">Username</span>
-                  <span className="text-gray-800 dark:text-white">@{userData?.userName}</span>
-                </div>
+              {/* Username */}
+              <div className="flex items-center justify-between bg-gray-100 dark:bg-gray-700 p-4 rounded-lg">
+                <span className="text-gray-600 dark:text-gray-300">Username</span>
+                <span className="text-gray-800 dark:text-white">@{userData?.userName}</span>
               </div>
             </div>
-            <Button onClick={() => setIsEditing(!isEditing)} className="mt-4">
-              {isEditing ? "Cancel" : "Edit Profile"}
-            </Button>
           </div>
-        )}
-        {isEditing && <EditableProfilePage userData={userData} />}
-      </>
-    );
-  } else {
-    return <AuthButton />;
-  }
+          <Button onClick={() => setIsEditing(!isEditing)} className="mt-4">
+            {isEditing ? "Cancel" : "Edit Profile"}
+          </Button>
+        </div>
+      )}
+      {isEditing && <EditableProfilePage userData={userData} />}
+    </>
+  );
+  // } else {
+  //   return <AuthButton />;
+  // }
 };
 
 export default Profile;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@components/layout/navbar/navbar";
@@ -6,9 +5,25 @@ import Navbar from "@components/layout/navbar/navbar";
 import { Toaster } from "react-hot-toast";
 const poppins = Poppins({ weight: ["100", "200", "300", "400", "500", "600", "700", "800"], subsets: ["latin", "latin-ext"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Socials",
-  description: "All your socials in one place"
+  description: "All your socials in one place",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  // keywords: ["nextjs", "nextjs14", "next14", "pwa", "next-pwa"],
+  // themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  // authors: [
+  //   { name: "Alldo Faiz Ramadhani" },
+  //   {
+  //     name: "Alldo Faiz Ramadhani",
+  //     url: "https://www.linkedin.com/in/alldofaiz/"
+  //   }
+  // ],
+  // viewport: "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  // icons: [
+  //   { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+  //   { rel: "icon", url: "icons/icon-128x128.png" }
+  // ]
 };
 
 export default function RootLayout({
