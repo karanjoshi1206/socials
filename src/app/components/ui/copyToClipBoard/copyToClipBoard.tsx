76d77b101aa2abd0a8f4da30a13200c24bd1fa8e
KARAN JOSHI <64606159+karanjoshi1206@users.noreply.github.com>
2024-09-26 14:12:28 +0530

    Update copyToClipBoard.tsx

--- src/app/components/ui/copyToClipBoard/copyToClipBoard.tsx
+++ src/app/components/ui/copyToClipBoard/copyToClipBoard.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 
 const CopyToClipBoard = ({ text = "" }) => {
+  console.log("copy text",text);
   const { showToast } = useToast();
   const handleCopy = () => {
     navigator.clipboard.writeText(text);
